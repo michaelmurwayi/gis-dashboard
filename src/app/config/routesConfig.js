2ebdb347a4da2d55fcdac7af5abdff90611a3a89
MichaelMurwayix <michael.murwayix@gmail.com>
2026-03-30 21:53:00 +0300

    ft-dashboard: Adding initial dashboard page component

---
+++ src/app/config/routesConfig.js
@@ -0,0 +1,9 @@
+import DashboardPage from "../../features/dashboard/pages/DashboardPage";
+
+export const routesConfig = [
+  {
+    path: "/",
+    name: "Dashboard",
+    component: DashboardPage,
+  },
+];
